e31537ac380284870df6ed99eb4a4435dec7c1c2
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-01 16:54:27 +0530

    Fix: use existing resource group with data block

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,17 @@
-resource "azurerm_resource_group" "aks_rg" {
-  name     = var.resource_group_name
-  location = var.location
+data "azurerm_resource_group" "aks_rg" {
+  name = var.resource_group_name
 }
 
 resource "azurerm_virtual_network" "aks_vnet" {
   name                = "aks-vnet"
   location            = var.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
+  resource_group_name = data.azurerm_resource_group.aks_rg.name
   address_space       = ["10.0.0.0/8"]
 }
 
 resource "azurerm_subnet" "aks_subnet" {
   name                 = "aks-subnet"
-  resource_group_name  = azurerm_resource_group.aks_rg.name
+  resource_group_name  = data.azurerm_resource_group.aks_rg.name
   virtual_network_name = azurerm_virtual_network.aks_vnet.name
   address_prefixes     = ["10.1.0.0/16"]
 }
@@ -20,7 +19,7 @@ resource "azurerm_subnet" "aks_subnet" {
 resource "azurerm_log_analytics_workspace" "log" {
   name                = "aks-log-workspace"
   location            = var.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
+  resource_group_name = data.azurerm_resource_group.aks_rg.name
   sku                 = "PerGB2018"
 }
 
@@ -29,7 +28,7 @@ module "aks" {
 
   cluster_name = var.cluster_name
   location = var.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
+  resource_group_name = data.azurerm_resource_group.aks_rg.name
   dns_prefix = "jdaks"
 
   node_count = var.node_count
