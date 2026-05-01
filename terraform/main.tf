1f538517bc38e12ebf1c425b789de42b5ea55531
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-01 18:31:01 +0530

    Switch to full Terraform resource creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,43 @@
-data "azurerm_resource_group" "aks_rg" {
-  name = var.resource_group_name
+resource "azurerm_resource_group" "aks_rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-data "azurerm_virtual_network" "aks_vnet" {
+resource "azurerm_virtual_network" "aks_vnet" {
   name                = "aks-vnet"
-  resource_group_name = var.resource_group_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  address_space       = ["10.0.0.0/8"]
 }
-data "azurerm_subnet" "aks_subnet" {
+
+resource "azurerm_subnet" "aks_subnet" {
   name                 = "aks-subnet"
-  virtual_network_name = "aks-vnet"
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = azurerm_resource_group.aks_rg.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.1.0.0/16"]
 }
-data "azurerm_log_analytics_workspace" "log" {
+
+resource "azurerm_log_analytics_workspace" "log" {
   name                = "aks-log-workspace"
-  resource_group_name = var.resource_group_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  sku                 = "PerGB2018"
 }
+
 module "aks" {
   source = "./modules/aks"
 
-  cluster_name = var.cluster_name
-  location = var.location
-  resource_group_name = data.azurerm_resource_group.aks_rg.name
-  dns_prefix = "jdaks"
+  cluster_name        = var.cluster_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "jdaks"
 
   node_count = var.node_count
   vm_size    = var.vm_size
 
-  subnet_id = data.azurerm_subnet.aks_subnet.id
-  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.log.id
+  subnet_id = azurerm_subnet.aks_subnet.id
+
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log.id
 
   tags = {
     Environment = var.environment
